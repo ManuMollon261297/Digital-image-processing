5985452d65ab6f9ce096907a3ee10b2af408b27e
taomasgonzalez <togonzalez@itba.edu.ar>
2020-08-19 21:07:04 -0300

    bilinear interpolation

--- TP1/code/main.py
+++ TP1/code/main.py
@@ -6,13 +6,14 @@ from ej_5 import run_ej5_a, run_ej5_b, run_ej5_c, run_ej5_d
 
 img = cv.imread(filename='../mono.bmp', flags=cv.IMREAD_GRAYSCALE)
 
-run_ej4()
-# run_ej5_a(img=img, show=True, same_size=False)
-# run_ej5_b(img=img, show=True, same_size=False)
+# run_ej4()
+# run_ej5_a(img=img, show=False, same_size=True)
+a = run_ej5_b(img=img, show=False, same_size=False)
 # a = run_ej5_c(img=img, show=False, method=None)
 # b = run_ej5_c(img=img, show=False)
 # new_mat = np.hstack((a, b))
-# run_ej5_d()
+b = run_ej5_d(a, show=False)
+c = cv.resize(src=a, dsize=(256, 256), fx=0, fy=0, interpolation=cv.INTER_LINEAR)
 
-# cv.imshow(winname='Averaged', mat=new_mat)
-# cv.waitKey(0)
+cv.imshow(winname='Averaged', mat=np.hstack((c, b)))
+cv.waitKey(0)
